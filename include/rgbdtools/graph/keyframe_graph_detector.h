8dada689e2d93db9f2afd8c65cabcb8952d2dd6f
Oleksandr Lavrushchenko <kpykcb@gmail.com>
2015-06-19 11:44:41 +0200

    chg: feature type setter param to const char*

--- include/rgbdtools/graph/keyframe_graph_detector.h
+++ include/rgbdtools/graph/keyframe_graph_detector.h
@@ -101,7 +101,7 @@ class KeyframeGraphDetector
       KeyframeAssociationVector& associations);
 
     // --------------------------------------------
-    void setFeaturesTypeForRANSAC(std::string* ransac_features_type_);
+    void setFeaturesTypeForRANSAC(const char* ransac_features_type_);
     void prepareFeaturesForRANSAC(KeyframeVector& keyframes);
     
     void buildAssociationMatrix(
